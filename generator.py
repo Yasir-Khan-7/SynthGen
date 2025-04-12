6daa7f26fbf84c9af794c4d84ad1308c40eb3128
Temp User <temp@example.com>
2025-04-12 18:16:22 +0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -8,9 +8,6 @@ from pydantic_ai.models.groq import GroqModel
 from pydantic_ai.providers.groq import GroqProvider
 from io import StringIO, BytesIO
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Set page configuration
 st.set_page_config(
@@ -850,10 +847,9 @@ st.markdown("""
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 """, unsafe_allow_html=True)
 
-groq_api_key = os.getenv("GROQ_API_KEY")
 # Initialize AI agent
 model = GroqModel(
-    'llama-3.3-70b-versatile', provider=GroqProvider(api_key=groq_api_key)
+    'llama-3.3-70b-versatile', provider=GroqProvider(api_key='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')
 )
 # model = GroqModel('llama-3.3-70b-versatile', api_key='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')
 agent = Agent(model)
